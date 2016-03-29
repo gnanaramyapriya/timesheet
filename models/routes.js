71cc005bf3131d9f8256b14bf1a94e82e5954bca
sanjana17 <navyasanjana4@gmail.com>
2016-03-29 12:06:30 -0500

    first dep

--- models/routes.js
+++ models/routes.js
@@ -1,11 +1,12 @@
 var mysql = require("mysql");
-
+var dateFormat = require('dateformat');
 // First you need to create a connection to the db
 var con = mysql.createConnection({
-  Host: "localhost",
-  user: "root",
-  password: "admin",
-  name: "navya"
+  host: "timesheet.ckqey0zkah3h.us-west-2.rds.amazonaws.com",
+  user: "navya",
+  password: "rajatarun",
+  name: "timesheet",
+  port:"3306"
 });
 
 con.connect(function(err){
@@ -19,8 +20,8 @@ con.connect(function(err){
 module.exports = function(app){
 	app.post("/api/login",function(req,res){
 		console.log(req.body);
-		con.query('use navya');
-		var query="select name, password from register where name='"+req.body.name+"'";
+		con.query('use timesheet');
+		var query="select username, password from register where username='"+req.body.name+"'";
 		con.query(query,function(err,resp){
 			if(err)
 				console.log(err);
@@ -29,11 +30,50 @@ module.exports = function(app){
 			}
 		});
 	})
+	app.post("/api/report",function(req,res){
+		var date1 = new Date(req.body.week1date).toISOString();
+		var week1 = dateFormat(date1,"W");
+		var year1 = dateFormat(date1,"yyyy");
+		var date2 = new Date(req.body.week2date).toISOString();
+		var week2 = dateFormat(date2,"W");
+		var year2 = dateFormat(date2,"yyyy");
+		var date3 = new Date(req.body.week3date).toISOString();
+		var week3 = dateFormat(date3,"W");
+		var year3 = dateFormat(date3,"yyyy");
+		var date4 = new Date(req.body.week4date).toISOString();
+		var week4 = dateFormat(date4,"W");
+		var year4 = dateFormat(date4,"yyyy");
+		query1 = "insert into "+req.body.username+" values("+"'"+req.body.username+"'"+","+week1+","+req.body.week1+","+year1+")";
+		query2 = "insert into "+req.body.username+" values("+"'"+req.body.username+"'"+","+week2+","+req.body.week2+","+year2+")";
+		query3 = "insert into "+req.body.username+" values("+"'"+req.body.username+"'"+","+week3+","+req.body.week3+","+year3+")";
+		query4 = "insert into "+req.body.username+" values("+"'"+req.body.username+"'"+","+week4+","+req.body.week4+","+year4+")";
+		con.query(query1,function(err,res){
+			if(err){
+				console.log(err);
+			}
+		})
+		con.query(query2,function(err,res){
+			if(err){
+				console.log(err);
+			}
+		})
+		con.query(query3,function(err,res){
+			if(err){
+				console.log(err);
+			}
+		})
+		con.query(query4,function(err,res){
+			if(err){
+				console.log(err);
+			}
+		})
+
+	})
 	app.post("/api/reg",function(req,res){
 		console.log(req.body);
 		var isValid = true;
 		var query="select email from register where email='"+req.body.email+"'";
-		con.query('use navya');
+		con.query('use timesheet');
 		con.query(query,function(err,resp){
 			if(err)
 				console.log(err);
@@ -46,8 +86,18 @@ module.exports = function(app){
 			}
 			catch(e){
 				if(e instanceof TypeError){
-				con.query('use navya');
-				var user = {name: req.body.name, email: req.body.email,password:req.body.password, repassword:req.body.repassword};
+				con.query('use timesheet');
+				console.log(req.body);
+
+				var query ="create table "+req.body.uname+" ( username varchar(15), weeknum int primary key, hours int, year int)";
+					console.log(query);
+				con.query(query,function(err,result){
+					if(err){
+						console.log(err);
+					}
+					console.log("success")
+				});
+				var user = {name: req.body.name, username: req.body.uname, email: req.body.email,password:req.body.password, repassword:req.body.repassword};
 				con.query('INSERT INTO register SET ?', user, function(err,response){
   				if(err) throw err;
  			 	console.log('Last insert ID:', response.insertId);
--- public/js/controllers/user.js
+++ public/js/controllers/user.js
@@ -1,5 +1,18 @@
 angular.module('app')
-.controller('userCntrl',['userDetails','$scope',function(userDetails,$scope){
+.controller('userCntrl',['userDetails','$scope','weeks',function(userDetails,$scope,weeks){
 	$scope.name=userDetails.getUsername();
-		
+	
+	$scope.num_week1= 1;
+	$scope.submit = function(timesheet){
+		$scope.timesheet.week1=$scope.timesheet.m1+$scope.timesheet.t1+$scope.timesheet.w1+$scope.timesheet.r1+$scope.timesheet.f1;
+		$scope.timesheet.week2=$scope.timesheet.m2+$scope.timesheet.t2+$scope.timesheet.w2+$scope.timesheet.r2+$scope.timesheet.f2;
+		$scope.timesheet.week3=$scope.timesheet.m3+$scope.timesheet.t3+$scope.timesheet.w3+$scope.timesheet.r3+$scope.timesheet.f3;
+		$scope.timesheet.week4=$scope.timesheet.m4+$scope.timesheet.t4+$scope.timesheet.w4+$scope.timesheet.r4+$scope.timesheet.f4;
+		$scope.timesheet.username = $scope.name;
+		console.log($scope.timesheet);
+		weeks.weekreport($scope.timesheet).then(function(res){
+			console.log(res.value);
+		})
+	}
+
 }])
